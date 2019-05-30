24aabe5048925fec53187aa68ab026de8e679b77
Revansiddh Patil <revansiddh.patil@coditas.com>
2019-05-30 12:14:49 +0530

    first commit ftp

---
+++ app/components/Counter.js
@@ -0,0 +1,68 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Counter.css';
+import routes from '../constants/routes';
+import SMB2 from 'smb2';
+import fs from 'fs-extra';
+
+type Props = {
+  increment: () => void,
+  incrementIfOdd: () => void,
+  incrementAsync: () => void,
+  decrement: () => void,
+  counter: number
+};
+
+export default class Counter extends Component<Props> {
+  props: Props;
+
+  handleSMB = () =>{
+    
+        // var smb2Client = new SMB2({
+    //   share: '\\\\192.168.4.221\\c$',
+    //   domain: 'React',
+    //   username: 'React',
+    //   password: 'react'
+    // });
+    // smb2Client.readFile('\\REACT-TESTING-S\\shared_folder\\a.txt',function(err, files) {
+    //   if (err) console.log('errir',err);
+    //   console.log('files',files);
+    // });
+    //   smb2Client.exists('shared_folder', function (err, exists) {
+    //     if (err) throw err;
+    //     console.log(exists ? "it's there" : "it's not there!");
+    // });
+    //     var SambaClient = require('samba-client');
+
+    //   var client = new SambaClient({
+    //     address: '192.168.4.221', // required
+    //     username: 'react', // not required, defaults to guest
+    //     password: 'react', // not required
+    //     domain: 'react' // not required
+    //   });
+    }
+
+  handleChange = async(event) => {
+    event.preventDefault();
+    // const sourcePath = '/home/revan/work/project1/src/styles'
+    try {
+        await fs.copy('G:\\Projects\\Source', 'G:\\Projects\\fsdest')
+    } catch (error) {
+        console.log('** Error **', error)
+    }
+    //   // get a file
+    //   client.getFile('shared_folder/A.TXT', './', function(err) {
+    //     console.log("***", err);
+    // })
+  };
+
+  render() {
+   
+    return (
+      <div>        
+        <button onClick={this.handleChange}>Copy</button>
+      </div>
+    );
+  }
+}
